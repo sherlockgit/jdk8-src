03f203175bb844b81f965913b07e57599a17829c
sherlock <1243935366@qq.com>
2025-06-23 10:10:17 +0800

    base

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * @Description:
+ * @Author: linmuyu
+ * @Date: 2025/6/21 11:56
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello jdk 1.8");
+    }
+
+}
---
+++ src/test/map/hashmap/Test.java
@@ -0,0 +1,30 @@
+package test.map.hashmap;
+
+
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * @Description:
+ * @Author: linmuyu
+ * @Date: 2025/6/21 15:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i = 1; i<50 ; i++){
+            if (i < 12) {
+                map.put(getKey(),i);//首次创建hashmap的数组+链表
+            }else if(i == 12){
+                map.put(getKey(),i);//首次创建hashmap的数组+链表
+            }
+        }
+
+    }
+
+    public static String getKey(){
+        return UUID.randomUUID().toString();
+    }
+
+}
